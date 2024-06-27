d10d88e873d88b930350c980b6b0082d513a10da
mayank0202 <mayank.sharma@sourcefuse.com>
2024-06-27 19:15:02 +0530

    removed commented out code

--- main.tf
+++ main.tf
@@ -308,16 +308,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
           storage_class             = noncurrent_version_transition.value.storage_class
         }
       }
-
-      # Max 1 block - filter - without any key arguments or tags
-      # dynamic "filter" {
-      #   for_each = length(try(flatten([rule.value.filter]), [])) == 0 ? [true] : []
-
-      #   content {
-      #     #          prefix = ""
-      #   }
-      # }
-
       # Max 1 block - filter - with one key argument or a single tag
       dynamic "filter" {
         for_each = [for v in try(flatten([rule.value.filter]), []) : v if max(length(keys(v)), length(try(rule.value.filter.tags, rule.value.filter.tag, []))) == 1]
