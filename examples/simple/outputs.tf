eefef8b6da001a7a6534eb064e33881e411055b6
vijay-stephen <vijay.stephen@sourcefuse.com>
2025-11-10 16:25:25 +0530

    tested the o/p

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -4,3 +4,13 @@ output "bucket_id" {
 output "bucket_arn" {
   value = module.s3.bucket_arn
 }
+
+output "bucket_domain_name" {
+  value       = module.s3.bucket_arn
+  description = "Bucket domain Name"
+}
+
+output "bucket_regional_domain_name" {
+  value       = module.s3.bucket_arn
+  description = "Bucket regional Domain Name"
+}
